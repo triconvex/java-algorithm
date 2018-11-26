dde0dc8900e94b7326602628ebdbca5a2c5fa877
zingoworks <zingoworks@gmail.com>
2018-11-26 23:06:06 +0900

    예산

--- programmers.co.kr/src/test/Level_1/예산Test.java
+++ programmers.co.kr/src/test/Level_1/예산Test.java
@@ -2,12 +2,39 @@ package Level_1;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class 예산Test {
     @Test
-    public void test() {
-        System.out.println(123);
+    public void testOne() {
+        int[] d = {1,3,2,5,4};
+        int budget = 9;
+        int answer = 0;
+
+        for (int i : Arrays.stream(d).sorted().toArray()) {
+            if(budget - i >= 0) {
+                budget -= i;
+                answer++;
+            }
+        }
+        assertEquals(answer, 3);
     }
 
+    @Test
+    public void testTwo() {
+        int[] d = {2,2,3,3};
+        int budget = 10;
+        int answer = 0;
+
+        for (int i : Arrays.stream(d).sorted().toArray()) {
+            if(budget - i >= 0) {
+                budget -= i;
+                answer++;
+            }
+        }
+        assertEquals(answer, 4);
+    }
 }
