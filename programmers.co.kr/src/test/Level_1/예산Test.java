cad4170dbeabd1a81742dea12aae4752f596e763
zingoworks <zingoworks@gmail.com>
2018-11-26 22:54:26 +0900

    init structure setting

---
+++ programmers.co.kr/src/test/Level_1/예산Test.java
@@ -0,0 +1,13 @@
+package Level_1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class 예산Test {
+    @Test
+    public void test() {
+        System.out.println(123);
+    }
+
+}
