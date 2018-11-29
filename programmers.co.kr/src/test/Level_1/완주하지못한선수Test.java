d6e0c10db6282ef4383332016e192dd117bcb242
zingoworks <zingoworks@gmail.com>
2018-11-29 23:11:13 +0900

    완주하지못한선수

---
+++ programmers.co.kr/src/test/Level_1/완주하지못한선수Test.java
@@ -0,0 +1,25 @@
+package Level_1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class 완주하지못한선수Test {
+    @Test
+    public void testOne() {
+        완주하지못한선수 resolve = new 완주하지못한선수();
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"kiki", "eden"};
+
+        assertEquals(resolve.solution(participant, completion), "leo");
+    }
+
+    @Test
+    public void testTwo() {
+        완주하지못한선수 resolve = new 완주하지못한선수();
+        String[] participant = {"mislav", "stanko", "mislav", "ana", "mislav", "stanko"};
+        String[] completion = {"stanko", "mislav", "ana", "mislav", "mislav"};
+
+        assertEquals(resolve.참고할만한_풀이(participant, completion), "stanko");
+    }
+}
