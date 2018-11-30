46eab7835e49be9b7b53ffdb7a7aa4767a531be1
zingoworks <zingoworks@gmail.com>
2018-12-01 08:32:34 +0900

    K번째수

---
+++ programmers.co.kr/src/test/Level_1/K번째수Test.java
@@ -0,0 +1,25 @@
+package Level_1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+
+
+public class K번째수Test {
+
+    @Test
+    public void solution() {
+        K번째수 resolve = new K번째수();
+        int[] array = {1,5,2,6,3,7,4};
+        int[][] commands = {{2,5,3}, {4,4,1}, {1,7,3}};
+        int[] answer = {5,6,3};
+
+        for (int i : answer) {
+            System.out.println(i);
+        }
+
+        for (int i : resolve.solution(array, commands)) {
+            System.out.println(i);
+        }
+    }
+}
