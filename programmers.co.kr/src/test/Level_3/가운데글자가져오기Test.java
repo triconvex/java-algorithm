241c07e623c0fa3e64ae9bf7a873196ac5440f53
zingoworks <zingoworks@gmail.com>
2018-12-05 18:09:00 +0900

    가운데글자가져오기

---
+++ programmers.co.kr/src/test/Level_3/가운데글자가져오기Test.java
@@ -0,0 +1,17 @@
+package Level_3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class 가운데글자가져오기Test {
+
+    @Test
+    public void solution() {
+        가운데글자가져오기 resolve = new 가운데글자가져오기();
+        String s = "abcde";
+
+        assertEquals(resolve.solution(s), "c");
+        assertEquals(resolve.참고할만한_풀이(s), "c");
+    }
+}
