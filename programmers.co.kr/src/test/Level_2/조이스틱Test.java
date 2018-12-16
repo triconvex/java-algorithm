8431f327b1a976272bf5bbb048fa3abc349379c6
zingoworks <zingoworks@gmail.com>
2018-12-16 17:39:56 +0900

    조이스틱

---
+++ programmers.co.kr/src/test/Level_2/조이스틱Test.java
@@ -0,0 +1,15 @@
+package Level_2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class 조이스틱Test {
+
+    @Test
+    public void solution() {
+        조이스틱 resolve = new 조이스틱();
+        String name = "JEROEN";
+        assertEquals(resolve.solution(name), 56);
+    }
+}
