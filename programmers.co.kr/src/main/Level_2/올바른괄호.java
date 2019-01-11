259cb6e863c8279b6cd21c92d78adc8aaa72af17
zingoworks <zingoworks@gmail.com>
2019-01-11 16:17:25 +0900

    올바른괄호

---
+++ programmers.co.kr/src/main/Level_2/올바른괄호.java
@@ -0,0 +1,23 @@
+package Level_2;
+
+public class 올바른괄호 {
+    boolean solution(String s) {
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                count++;
+            } else {
+                count--;
+            }
+
+            if(count < 0) {
+                return false;
+            }
+        }
+
+        return count == 0;
+    }
+}
+
+//다른풀이는 없을까?
